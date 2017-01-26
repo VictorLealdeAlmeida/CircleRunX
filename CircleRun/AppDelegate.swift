cd4bb21e12f29c709561668c1f743bae6c11265d
VictorLealdeAlmeida <vlpaa@cin.ufpe.br>
2017-01-25 22:14:05 -0300

    Initial Commit

---
+++ CircleRun/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  CircleRun
+//
+//  Created by Victor Leal Porto de Almeida Arruda on 25/01/17.
+//  Copyright © 2017 VTR. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
